b5665077f31345bc61713eeaea530a147b6e4b04
Andrey <Andrey-Carlos@outlook.com>
2025-09-02 19:47:31 -0300

    Feat: Adicionado migração automática

--- Financecontrol.WebApi/Program.cs
+++ Financecontrol.WebApi/Program.cs
@@ -161,4 +161,10 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<FinanceDbContext>();
+    db.Database.Migrate();
+}
+
 app.Run();
